2f68c0a41ba1d0a2837dc55ca428da6cc049b51f
bingerzhangbing <151362909@qq.com>
2017-11-03 17:09:07 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <!-- footer -->
-<footer class="footer">
+<footer class="footer" style="position:fixed; bottom:0;width: 100%;">
   <p>© 2016 By <a href="http://www.shiyanlou.com" target="_blank">www.shiyanlou.com</a></p>
 </footer>
