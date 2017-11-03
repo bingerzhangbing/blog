4d5adff6770ef97bc01582f6146dc75302a70991
151362909 <151362909@qq.com>
2017-11-03 15:20:26 +0800

    vendor_temp

---
+++ vendor_tmp/alfredo-ramos/parsedown-extra-laravel/src/ParsedownExtraLaravel.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Copyright (C) 2015 Alfredo Ramos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AlfredoRamos\ParsedownExtra;
+
+use ParsedownExtra;
+use Purifier;
+use Config;
+
+class ParsedownExtraLaravel extends ParsedownExtra {
+
+	/**
+	 * @deprecated Function overloaded to maintain compatibility.
+	 * @see \Parsedown::parse()
+	 *
+	 * @param string $text
+	 * @param array $options
+	 *
+	 * @return string
+	 */
+	public function parse($text, $options = []) {
+		// Extend default options
+		$options = array_merge([
+			'config'	=> 'parsedown',
+			'purifier'	=> true
+		], $options);
+
+		// Parsedown Extra
+		$markdown = parent::text($text);
+
+		// HTML Purifier
+		if (Config::get('parsedownextra.purifier.enabled') && $options['purifier']) {
+			// HTML Purifier configuration
+			if (is_string($options['config']) && !empty($options['config'])) {
+				$options['config'] = Config::get(sprintf(
+					'parsedownextra.purifier.settings.%s',
+					$options['config']
+				));
+			}
+
+			// Filter HTML
+			$markdown = Purifier::clean($markdown, $options['config']);
+		}
+
+		return $markdown;
+	}
+
+}
---
+++ vendor_tmp/alfredo-ramos/parsedown-extra-laravel/src/ParsedownExtraServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright (C) 2015 Alfredo Ramos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AlfredoRamos\ParsedownExtra;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class ParsedownExtraServiceProvider extends ServiceProvider {
+
+	/**
+	 * Perform post-registration booting of services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		$this->publishes([
+			__DIR__ . '/../config/parsedownextra.php' => config_path('parsedownextra.php')
+		], 'config');
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->mergeConfigFrom(
+			__DIR__ . '/../config/parsedownextra.php', 'parsedownextra'
+		);
+
+		$this->app->singleton(ParsedownExtraLaravel::class, function($app){
+			return new ParsedownExtraLaravel;
+		});
+
+		// Register HTML Purifier
+		$this->app->register(\Mews\Purifier\PurifierServiceProvider::class);
+		$loader = AliasLoader::getInstance();
+		$loader->alias('Purifier', \Mews\Purifier\Facades\Purifier::class);
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides() {
+		return [ParsedownExtraLaravel::class];
+	}
+
+}
